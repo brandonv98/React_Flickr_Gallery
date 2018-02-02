d821419918317cc26981ca4ffef42b0f375b18b8
brandonv98 <enjoi98@gmail.com>
2018-02-01 18:56:40 -0600

    Clean Read

--- react_flikr_gallery/src/components/MainNav.js
+++ react_flikr_gallery/src/components/MainNav.js
@@ -3,7 +3,10 @@ import React from 'react';
 const MainNav = (props) => {
 
 	const navData = props.navNames; //navNames data
-	let links = navData.map(name => <li key={name.toString()}><a onClick={props.handleClick}>{name} </a></li>);
+	let links = navData.map(name =>
+		<li key={name.toString()}>
+			<a onClick={props.handleClick}>{name}
+		</a></li>);
 // add the links dynamicly form "navName" state
 		return (
 			<nav className="main-nav">
--- react_flikr_gallery/src/components/ResultList.js
+++ react_flikr_gallery/src/components/ResultList.js
@@ -8,7 +8,10 @@ const ResultList = props => {
 	//																														https:// + farm{farm-id} + .staticflickr.com/ + {server-id} + / + {id} + _ + {secret} + .jpg
 	const results = props.data;
 	// Loop threw photos data
-	let photos = results.map(photo => <Photo key={photo.id} url={'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg'}/>);
+	let photos = results.map(photo =>
+		<Photo
+			key={photo.id}
+			url={'https://farm' + photo.farm + '.staticflickr.com/' + photo.server + '/' + photo.id + '_' + photo.secret + '.jpg'}/>);
 
 // Test for 404 error code
 	const isNoResults = results.length;
